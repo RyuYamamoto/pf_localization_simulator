0038916ec83069832c14fdd3f82b7b8e2c4e05c2
RyuYamamoto <ryu.yamamoto@tier4.jp>
2021-08-01 17:38:19 +0900

    add pdf function

--- include/particle_filter_localization/multi_variate_generator.h
+++ include/particle_filter_localization/multi_variate_generator.h
@@ -12,14 +12,20 @@ public:
     Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eigen_solver(covariance_);
     transform_ = eigen_solver.eigenvectors() * eigen_solver.eigenvalues().cwiseSqrt().asDiagonal();
   }
+  double pdf(const Eigen::VectorXd & x) const
+  {
+    double n = x.rows();
+    double a = (x - mean_).transpose() * covariance_.inverse() * (x - mean_);
+    double b = std::pow(std::sqrt(2 * M_PI), n) * std::sqrt(covariance_.determinant());
+    return b * std::exp(-1 * a / 2.0);
+  }
   Eigen::VectorXd operator()() const
   {
     static std::random_device seed;
     static std::mt19937 engine(seed());
     static std::normal_distribution<> dist;
 
-    return mean_ + transform_ *
-                     Eigen::VectorXd{mean_.size()}.unaryExpr([&](auto x) { return dist(engine); });
+    return mean_ + transform_ * Eigen::VectorXd{mean_.size()}.unaryExpr([&](auto x) { return dist(engine); });
   }
   void setMean(const Eigen::VectorXd mean) { mean_ = mean; }
   void setCovariance(const Eigen::MatrixXd covariance) { covariance_ = covariance; }
